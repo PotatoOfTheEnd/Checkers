3bc293b0dcda279ab452eab7fac2acb644ecec61
DoomPotato <shaar@utschools.ca>
2017-05-04 12:54:31 -0400

    start making start page

---
+++ app/src/main/java/com/example/arielle/checkers/StartPage.java
@@ -0,0 +1,27 @@
+package com.example.arielle.checkers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.arielle.checkers.R.styleable.View;
+
+/**
+ * Created by arielle on 04/05/2017.
+ */
+
+public class StartPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+        Button mCheckersButton = (Button) findViewById(R.id.checkersButton);
+        mCheckersButton.setOnClickListener(new android.view.View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+    }
+}
