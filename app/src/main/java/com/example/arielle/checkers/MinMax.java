4ae7e0e90b7ce08a7c15f91d2faeaa13e654388b
DoomPotato <shaar@utschools.ca>
2017-05-17 14:06:05 -0400

    modify minmax so that it chooses randomly from moves of equal value instead of doing the same thing each time

--- app/src/main/java/com/example/arielle/checkers/MinMax.java
+++ app/src/main/java/com/example/arielle/checkers/MinMax.java
@@ -12,6 +12,7 @@ public class MinMax {
     public static Move getVal(gameBoard currentBoard, int player, int depth, int alpha, int beta){
         Move finalMove = currentBoard.getMove();
         int bestScore, opponent;
+        double rnd = Math.random();
         gameBoard tpBoard;
         if (player == 1) {
             opponent = 2;
@@ -38,16 +39,34 @@ public class MinMax {
                 int tmp = getVal(tpBoard, opponent, depth-1, alpha, beta).getScore();
 
                 if (player == 1) {
-                    if (tmp >= bestScore){
+                    if (tmp==bestScore){
+                        double tprand = Math.random();
+                        if (tprand>rnd){
+                            finalMove = j;
+                            bestScore = tmp;
+                            rnd=tprand;
+                        }
+                    }
+                    else if (tmp > bestScore){
                         finalMove = j;
                         bestScore = tmp;
+                        rnd = Math.random();
                     }
                     alpha = Math.max(alpha, bestScore);
                 }
                 else {
-                    if (tmp <= bestScore){
+                    if (tmp==bestScore){
+                        double tprand = Math.random();
+                        if (tprand>rnd){
+                            finalMove = j;
+                            bestScore = tmp;
+                            rnd=tprand;
+                        }
+                    }
+                    else if (tmp < bestScore){
                         finalMove = j;
                         bestScore = tmp;
+                        rnd = Math.random();
                     }
                     beta = Math.min(beta, bestScore);
                 }
